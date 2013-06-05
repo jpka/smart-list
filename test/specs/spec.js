2311cd5478f8f2b5998bcdd4acbe86cbfc31ab35
Juan Pablo Kaniefsky <jpkaniefsky@gmail.com>
2013-06-04 21:51:07 -0300

    First

--- test/specs/spec.js
+++ test/specs/spec.js
@@ -1,7 +1,42 @@
-describe("my-element", function() {  
+describe("element-collection", function() {  
   var element;
 
-  beforeEach(function() {
-    element = fixtures.window().document.createElement("my-element");
+  before(function() {
+    element = fixtures.window().document.createElement("jpka-elements-collection");
+  });
+
+  it("has a list", function() {
+    expect(element.$.list).to.exist;
+  });
+
+  describe("addBatch()", function() {
+    var models;
+
+    before(function() {
+      models = ["a", "b", "c"].map(function(text) {
+        return {
+          model: {
+            body: text
+          }
+        };
+      });
+      element.addBatch("single-element", models);
+    });
+
+    it("adds a batch of elements, attaching properties to them", function() {
+      expect(element.elements.length).to.equal(3);
+      element.elements.forEach(function(ele) {
+        expect(ele.tagName).to.equal("SINGLE-ELEMENT");
+        expect(ele.model).to.have.property("body");
+      });
+    });
+
+    it("also aggregates them to the list as list items", function() {
+      expect(element.$.list.childNodes.length).to.equal(element.elements.length);
+      element.$.list.childNodes.forEach(function(ele) {
+        expect(ele.tagName).to.equal("LI");
+        expect(ele.childNodes[0].tagName).to.equal("SINGLE-ELEMENT");
+      });
+    });
   });
 });
