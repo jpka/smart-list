b01a38c395a5acbfad12857e0b70b82ea724b8e7
Juan Pablo Kaniefsky <jpkaniefsky@gmail.com>
2013-06-07 10:37:11 -0300

    Refactored to extend ul, as it always should have

--- test/specs/spec.js
+++ test/specs/spec.js
@@ -5,13 +5,6 @@ describe("sl", function() {
     element = fixtures.window().document.createElement("jpka-sl");
   });
 
-  describe("items property", function() {
-    it("gets", function() {
-      element.$.list.appendChild(document.createElement("div"));
-      expect(element.items).to.deep.equal(element.$.list.childNodes);
-    });
-  });
-
   it("addBatch() adds a batch of elements, attaching properties to them", function() {
     var models = ["a", "b", "c"].map(function(text) {
       return {
@@ -22,7 +15,7 @@ describe("sl", function() {
     }),
     childs;
     element.addBatch("single-element", models);
-    childs = element.$.list.childNodes;
+    childs = element.childNodes;
 
     expect(childs.length).to.equal(3);
     childs.forEach(function(ele) {
